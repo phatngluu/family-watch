3dc51b9acb4f2b49814b8667c95b9717ba2053f9
phatngluu <yadsendew@protonmail.com>
2021-02-28 21:35:36 +0700

    before change from mongoose to mongodb

--- server/server.js
+++ server/server.js
@@ -1,14 +1,51 @@
 require('dotenv').config()
+const express = require('express')
+const { spawn } = require('child_process')
 const WebSocket = require('ws')
 const { DBConnection, UserData } = require('./database.js');
 
+const app = express()
+const port = 3000
+
 const wss = new WebSocket.Server({ port: 8080 })
 let tempDataMap = new Map();
+
+/** API */
+app.get('/', (req, res) => {
+    console.log("Analysing...")
+    
+})
+
 
 /** DB Connection */
 DBConnection.on('error', console.error.bind(console, 'Database connection error:'))
 DBConnection.once('open', () => {
     console.log('Database connected.')
+
+    /** Websocket */
+    wss.on('connection', ws => {
+        ws.on('message', message => {
+            console.log(`Received message => ${message}`)
+            let [customerID, stepCount, heartRate, timestamp] = message.split('/')
+            stepCount = parseInt(stepCount)
+            heartRate = parseInt(heartRate)
+            timestamp = Date(Math.round(parseFloat(timestamp)))
+    
+            if (tempDataMap.has(customerID)) {
+                let prevHeartRate = tempDataMap.get(customerID).heartRate
+                heartRate = Math.round((heartRate + prevHeartRate) / 2)
+            }
+            tempDataMap.set(customerID, {
+                customerID,
+                stepCount,
+                heartRate,
+                timestamp
+            })
+        })
+        ws.send('Connected to server!')
+    })
+
+    /** Every 2 minutes send data to datatbase */
     setInterval(() => {
         tempDataMap.forEach( async (element, key) => {
             console.log(element);
@@ -36,27 +73,9 @@ DBConnection.once('open', () => {
             console.log('User data sent.');
         }
     }, parseInt(process.env.DB_TIME_SEND))
-})
 
-
-wss.on('connection', ws => {
-    ws.on('message', message => {
-        console.log(`Received message => ${message}`)
-        let [customerID, stepCount, heartRate, timestamp] = message.split('/')
-        stepCount = parseInt(stepCount)
-        heartRate = parseInt(heartRate)
-        timestamp = Date(Math.round(parseFloat(timestamp)))
-
-        if (tempDataMap.has(customerID)) {
-            let prevHeartRate = tempDataMap.get(customerID).heartRate
-            heartRate = Math.round((heartRate + prevHeartRate) / 2)
-        }
-        tempDataMap.set(customerID, {
-            customerID,
-            stepCount,
-            heartRate,
-            timestamp
-        })
+    /** Start webserver */
+    app.listen(port, () => {
+        console.log("Web server is listening on port " + port);
     })
-    ws.send('ho!')
 })
